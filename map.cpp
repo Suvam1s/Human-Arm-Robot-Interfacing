673001ee953976a02b59e614f0734f13103a12f6
Suvam1s <a1234567890suvam@gmail.com>
2026-05-27 07:55:19 +0000

    uupdate map.cpp

---
+++ map.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "map.h"
+
+using namespace std;
+
+void loadMap() {
+
+    cout << "Loading warehouse map..." << endl;
+
+    cout << "[ ][ ][X][ ]" << endl;
+    cout << "[ ][X][ ][ ]" << endl;
+    cout << "[ ][ ][ ][G]" << endl;
+}
+
