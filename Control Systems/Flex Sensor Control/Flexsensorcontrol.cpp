5c396e51463cd1b04ca72b5ae7de55ba573aa5d8
Suvam1s <144379163+Suvam1s@users.noreply.github.com>
2026-07-21 12:35:12 +0530

    Update Flexsensorcontrol.cpp

--- Control Systems/Flex Sensor Control/Flexsensorcontrol.cpp
+++ Control Systems/Flex Sensor Control/Flexsensorcontrol.cpp
@@ -1,1 +1,68 @@
+#include <ESP32Servo.h>
 
+Servo elbow;
+
+const int sensorPin = 34;
+const int servoPin = 18;
+
+// Calibration values
+const int adcStraight = 620;
+const int adcBent     = 3720;
+
+// Exponential Moving Average
+const float alpha = 0.15;
+
+float filtered = 0;
+
+int currentAngle = 90;
+const int servoSpeed = 2;
+
+void setup()
+{
+    Serial.begin(115200);
+
+    analogReadResolution(12);        // 0-4095
+    analogSetAttenuation(ADC_11db);  // ~0-3.3V input
+
+    elbow.setPeriodHertz(50);
+    elbow.attach(servoPin, 500, 2500);
+
+    filtered = analogRead(sensorPin);
+
+    elbow.write(currentAngle);
+}
+
+void loop()
+{
+    int raw = analogRead(sensorPin);
+
+    filtered = alpha * raw + (1 - alpha) * filtered;
+
+    int target = map(
+        filtered,
+        adcStraight,
+        adcBent,
+        0,
+        180
+    );
+
+    target = constrain(target, 0, 180);
+
+    if(currentAngle < target)
+        currentAngle += servoSpeed;
+    else if(currentAngle > target)
+        currentAngle -= servoSpeed;
+
+    elbow.write(currentAngle);
+
+    Serial.print("ADC = ");
+    Serial.print(raw);
+
+    Serial.print("  Filtered = ");
+    Serial.print(filtered);
+
+    Serial.print("  Angle = ");
+    Serial.println(currentAngle);
+
+    delay(15);
+}
